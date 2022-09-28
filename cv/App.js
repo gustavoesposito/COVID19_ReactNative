f0a490d258619fc40a7366861d9351cbbbe54c05
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-27 21:03:50 -0300

    att

--- cv/App.js
+++ cv/App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react'
+import { StyleSheet, Text, SafeAreaView, FlatList } from 'react-native';
+import React, {Component}from 'react'
+import api from './src/services/api'
+ import Paises from './src/pages/Paises';
+ 
 
-export default function App() {
+ 
+
+export default class App extends Component(){
+
+  constructor(props){
+    super(props)
+    this.state = {
+      paises:[]
+    }
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/paises')
+    this.setState({
+      paises: response.data
+    })
+  }
+
+  render(){ 
   return (
-    <View style={styles.container}>
-      <Text>Ola Mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data={this.state.paises}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => <Paises/>}
+      />
+  
+    </SafeAreaView>
   );
+}
 }
 
 const styles = StyleSheet.create({
---
+++ cv/src/pages/Paises/index.js
@@ -0,0 +1,12 @@
+import React from'react'
+import { View, Text, Image } from 'react-native'
+
+export default class Paises extends Component{
+    render(){
+        return(
+            <View>
+                
+            </View>
+        )
+    }
+}
