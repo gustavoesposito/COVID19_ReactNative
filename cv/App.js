3ec44da8de2d2cfc95fc8f8ef4e9351a5ba76578
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-28 00:22:05 -0300

    att

--- cv/App.js
+++ cv/App.js
@@ -1,7 +1,11 @@
- import React, {useEffect, useState} from 'react'
- import {SafeAreaView, Text, Image} from 'react-native'
+ import { response } from 'express'
+import React, {useEffect, useState} from 'react'
+ import {SafeAreaView,  View, Text,  Image,FlatList} from 'react-native'
+ 
 
  export default function App(){
+
+  const[country, setCountry] = useState([])
 
   useEffect(() => {
     fetch('https://api.covid19api.com/countries',{
@@ -10,11 +14,36 @@
     "Accept": 'aplication/json '
   }
     })
+    .then(response => response.json())
+    .then(data => {
+      setCountry(data.results)
+    })
   }, [])
 
 return(
    <SafeAreaView>
-    
+    <FlatList
+    data={country}
+    keyExtractor={(country) => country.name}
+    contentContainerStyle={{ flexGrow: 1}}
+    renderItem={showCountry}
+    />
    </SafeAreaView>
 )
+ }
+
+ function showCountry(item){
+
+  const {name, url} = item.item
+
+  const isoCountry = url.replace('https://countryflagsapi.com/:filetype/:code', '').replace()
+  
+  return(
+<View>
+  <Image
+
+  />
+  <Text> {name} </Text>
+</View>
+  )
  }
